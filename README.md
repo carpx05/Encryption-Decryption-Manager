9f350b096fd9f812e377f640b24bf82bc3a33b71
Ayush <aa1465@srmist.edu.in>
2023-08-06 21:08:52 +0530

    version update.

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# Encryption-Decryption-Manager
-OS Miniproject for college semester 3
+# Encryption and Decryption Manager
+
+This repository contains a Python script for an Encryption and Decryption Manager that supports both the Affine Cipher and Vigenere Cipher encryption algorithms. The manager provides a user-friendly interface for encrypting and decrypting messages using these cryptographic methods.
+
+# Features
+
+User-friendly graphical interface for encryption and decryption operations.
+Support for both Affine Cipher and Vigenere Cipher algorithms.
+Choose between encryption and decryption modes.
+Choose between plaintext and cipher text input.
+
+# Getting Started
+
+To use the Encryption and Decryption Manager, follow these steps:
+
+Clone the repository to your local machine:
+
+    git clone https://github.com/your-username/encryption-decryption-manager.git
+    cd encryption-decryption-manager
+
+Make sure you have the required dependencies installed. You can install them using:
+
+    pip install easygui
+
+Run the script:
+
+    python encryption_decryption_manager.py
+
+The graphical interface will launch, allowing you to select the encryption/decryption mode, the cryptographic algorithm, and input text/key.
+
+# Usage
+
+Launch the script using the instructions provided in the "Getting Started" section.
+
+The main menu will be displayed. Choose between encryption and decryption modes.
+
+Select the cryptographic algorithm you want to use: Affine Cipher or Vigenere Cipher.
+
+Choose whether you want to input plaintext or cipher text.
+
+Enter the required input text and key as prompted.
+
+The script will display the original and encrypted/decrypted messages.
+
+# Contributing
+
+Contributions are welcome! If you find any issues or have suggestions for improvements, feel free to open an issue or submit a pull request.
+
+# Acknowledgments
+
+The Encryption and Decryption Manager was developed using the easygui library for creating the graphical user interface.
---
+++ main.py
@@ -0,0 +1,69 @@
+from easygui import buttonbox, multpasswordbox
+from itertools import starmap, cycle
+from vigenere_cipher import vigenere_cipher
+from affine_cipher import affine_cipher
+
+
+
+title = "OS Encryption and Decryption Manager"
+
+
+def main():
+    msg1 = "Welcome, please pick a cryptosystem."
+    crypto_choices = ["Encryption", "Decryption"]
+    crypto_mode = buttonbox(msg1, title, choices=crypto_choices)
+
+    msg2 = "Please pick a crypto mode to use."
+    cryptosystem_choicess = ["Affine Cipher", "Vigenere Cipher"]
+    cryptosystem_mode = buttonbox(msg2, title, choices=cryptosystem_choicess)
+
+    msg3 = "Would you like plaintext or cipher text?"
+    text_choices = ["Plaintext", "Cipher text"]
+    text_mode = buttonbox(msg3, title, choices=text_choices)
+
+    msg4 = "Please enter your text and key."
+    if cryptosystem_mode == 'Affine Cipher':
+        field_names = ["Text", "A", "B"]
+        field_values = multpasswordbox(msg4, title, field_names)
+        # make sure that none of the fields was left blank
+        while 1:
+            if field_values is None:
+                break
+            errmsg = ""
+            for i in range(len(field_names)):
+                if field_values[i].strip() == "":
+                    errmsg = errmsg + ('"%s" is a required field.\n\n' % field_names[i])
+            if errmsg == "":
+                break  # no problems found
+            field_values = multpasswordbox(errmsg, title, field_names, field_values)
+
+        my_text = field_values[0]
+        a = field_values[1]
+        b = field_values[2]
+        print("[Affine Cipher]")
+        print('%s message' % (crypto_mode.title()))
+        print(affine_cipher(a, b, my_text, crypto_mode, text_mode))
+
+    elif cryptosystem_mode == 'Vigenere Cipher':
+        field_names = ["Text", "Key"]
+        field_values = multpasswordbox(msg4, title, field_names)
+        # make sure that none of the fields was left blank
+        while 1:
+            if field_values is None:
+                break
+            errmsg = ""
+            for i in range(len(field_names)):
+                if field_values[i].strip() == "":
+                    errmsg = errmsg + ('"%s" is a required field.\n\n' % field_names[i])
+            if errmsg == "":
+                break  # no problems found
+            field_values = multpasswordbox(errmsg, title, field_names, field_values)
+
+        my_text = field_values[0]
+        my_key = field_values[1]
+        print("[Vigenere Cipher]")
+        print('%s message' % (crypto_mode.title()))
+        print(vigenere_cipher(my_key, my_text, crypto_mode, text_mode))
+
+if __name__ == '__main__':
+    main()
